15086d32c8b675cc63ab375a589efac34f9f6d66
Prince Sinha <104667459+Prince-Sinha@users.noreply.github.com>
2024-02-16 01:51:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const User = require('./Models/userModel');
 
      
 
-const DB = process.env.DATABASE;
+const DB = process.env.DATABASE.replace('<password>', process.env.PASSWORD);
 
 mongoose.connect(DB,{}).then(()=> console.log('DB connect successfully..'));
 
